20c652a46bdd1285025394d2c7ce57e285a481c0
Vaishnavi Nighvekar <86281222+VaishnaviNighvekar@users.noreply.github.com>
2022-09-23 11:55:08 -0700

    Add files via upload

---
+++ Resources/c.bat
@@ -0,0 +1,8 @@
+C:
+cd C:\Users\Vaishnavi nighvekar\PycharmProjects\pythonProject\editor\
+ nighvekar\PycharmProjects\pythonProject\editor\
+ nighvekar\PycharmProjects\pythonProject\editor\
+ nighvekar\PycharmProjects\pythonProject\editor\
+gcc vaishu.c -o vaishu
+vaishu
+pause
---
+++ Resources/file.bat
@@ -0,0 +1,17 @@
+E:
+cd E:\Java\java project\java project\
+ project\java project\
+ project\java project\
+ project\java project\
+ project\java project\
+ project\
+ project\
+ project\
+ project\
+ project\java project\
+ Project GPA\
+ nighvekar\PycharmProjects\pythonProject\editor\
+ nighvekar\PycharmProjects\pythonProject\editor\
+javac Editor.java
+java Editor
+pause
